72f47b038e919d519925ee92aad4ca4f5bc80c63
Misha373 <misasapka1@gmail.com>
2024-10-03 21:07:27 +0300

    Добавьте файлы проекта.

---
+++ HW14.1/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14._1
+{
+    public class Account
+    {
+        private int money;
+        private string name;
+
+
+        public Account(int money, string name)
+        {
+            this.money = money > 0 ? money:0;
+            this.name = name;
+        }
+        public int Deposit(int amount)
+        {
+            return money += amount;
+        }
+        public int Withdrawal(int amount)
+        {
+            return money -= amount;
+        }
+        public int Balance()
+        {
+            return money;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
---
+++ HW14.1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HW14._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account(100, "Misha");
+            Account account2 = new Account(50, "Alex");
+            account1.Withdrawal(20);
+            Console.WriteLine($"The balance of {account1.GetName()} account is now: {account1.Balance()}");
+            account2.Deposit(200);
+            Console.WriteLine($"The balance of {account2.GetName()} account is now: {account2.Balance()}");
+        }
+    }
+}
